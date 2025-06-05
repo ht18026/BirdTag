9c650951172b08fb78cbfd8383a130920e3b25ff
Page <xwan0458@student.monash.edu>
2025-06-05 16:18:29 +1000

    video detection finalised

--- lambda_video_detection/lambda_function.py
+++ lambda_video_detection/lambda_function.py
@@ -5,9 +5,39 @@ from collections import Counter, defaultdict
 from ultralytics import YOLO
 import supervision as sv
 import cv2 as cv
-from utils import download_file_from_s3, get_model_path
+import boto3
 
+s3 = boto3.client("s3")
+MODEL_PREFIX = "models/"
+REGION = "us-east-1"
+DDB_TABLE = "bird-db"
+MODEL_ENV = os.environ.get("MODEL_NAME", "model.pt")
+BUCKET_NAME = os.environ.get("BUCKET_NAME", "birdtag-models-fit5225-g138")
 
+def get_model_path():
+    tmp_path = os.path.join("/tmp", MODEL_ENV)
+
+    if not os.path.exists(tmp_path):
+        print(f"Downloading model {MODEL_ENV} from s3://{BUCKET_NAME}/{MODEL_ENV}")
+        s3.download_file(BUCKET_NAME, MODEL_PREFIX + MODEL_ENV, tmp_path)
+    else:
+        print("Model already cached in /tmp")
+
+    return tmp_path
+
+def write_to_dynamodb(media_id, species_count, file_type, full_url, thumb_url):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(DDB_TABLE)
+    with table.batch_writer() as batch:
+        for bird_tag, count in species_count.items():
+            batch.put_item(Item={
+                'media_id': media_id,
+                'bird_tag': bird_tag,
+                'count': count,
+                'file_type': file_type,
+                'full_url': full_url,
+                'thumb_url': thumb_url
+            })
 def video_prediction(video_path,model, confidence=0.5,
                      ):
     """
@@ -60,7 +90,6 @@ def video_prediction(video_path,model, confidence=0.5,
     print("Max species count per frame:", dict(max_species_counts))
     return dict(max_species_counts)
 
-
 def handler(event, context):
     try:
         record = event["Records"][0]
@@ -72,9 +101,29 @@ def handler(event, context):
     print(f"[INFO] New file uploaded to S3: {bucket}/{key}")
 
     video_path = f"/tmp/{os.path.basename(key)}"
+    file_ext = os.path.splitext(key)[1].lower()
+    file_type = "video" if file_ext in [".mp4", ".mov", ".avi", ".mkv"] else "unknown"
 
-    download_file_from_s3(bucket, key, video_path)
+    #download image from s3
+    s3.download_file(bucket, key, video_path)
+
+    #video detection
     model_path = get_model_path()
-    result = video_prediction(video_path,model_path)
+    species_count = video_prediction(video_path,model_path)
 
-    return {"tag": result}
+    #construct urls
+    full_url = f"https://{bucket}.s3.{REGION}.amazonaws.com/{key}"
+
+    #Write to DynamoDB
+    write_to_dynamodb(
+        media_id=key,
+        species_count=species_count,
+        file_type=file_type,
+        full_url=full_url,
+        thumb_url="N/A"
+    )
+
+    return {"message": "Processing complete", "tags": species_count}
+
+
+
